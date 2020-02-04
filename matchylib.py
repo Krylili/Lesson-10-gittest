2c690dfb6eecac523b73fe8f96db8cb7f3f1dfcc
Sven Kochmann <skochman@yorku.ca>
2020-02-04 18:51:48 -0500

    Added basic match function

--- matchylib.py
+++ matchylib.py
@@ -6,3 +6,9 @@ Created on Tue Feb  4 18:41:52 2020
 @author: Presenter
 """
 
+def is_base_match(base1: str, base2: str) -> bool:
+    if base1 == base2:
+        return True
+    else:
+        return False
+    
