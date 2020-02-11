1985f5ed8bbef9878ec807ea06660817e5692aa5
Sven Kochmann <skochman@yorku.ca>
2020-02-11 18:03:56 -0500

    Added N matches everything case

--- matchylib.py
+++ matchylib.py
@@ -12,6 +12,9 @@ BASEMATCH_CMP_T_AND_U = 1
 def is_base_match(base1: str, base2: str) -> (bool, int):    
     if (base1 == "T" and base2 == "U") or (base2 == "T" and base1 == "U"):
         return False, BASEMATCH_CMP_T_AND_U 
+    
+    if base1 == "N" or base2 == "N":
+        return True, BASEMATCH_NOERROR
 
     return base1 == base2, BASEMATCH_NOERROR
 
@@ -21,8 +24,10 @@ if __name__ == "__main__":
     print(is_base_match("A", "A"))
     print(is_base_match("C", "C"))
     print(is_base_match("G", "G"))
-    print(is_base_match("T", "T"))
+    print(is_base_match("T", "T"))    
     
+    print(is_base_match("A", "N"), "A matches or not matches any base.")
+    print(is_base_match("X", "N"), "X matches or not matches any base.")
     
     print(is_base_match("T", "U"), "should be false, but unique case that should not happen anyway.")
     
