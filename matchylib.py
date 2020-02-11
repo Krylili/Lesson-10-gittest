356f4ed34277eb4ae65314811208b5365e25a1af
Sven Kochmann <skochman@yorku.ca>
2020-02-11 18:33:46 -0500

    Uncertain vs certain cases

--- matchylib.py
+++ matchylib.py
@@ -8,12 +8,19 @@ Created on Tue Feb  4 18:41:52 2020
 
 BASEMATCH_NOERROR = 0
 BASEMATCH_CMP_T_AND_U = 1
+BASEMATCH_UNCERTAIN = 2
 
-BASEMATCH_DICT = {"Y": ["C", "T"], "R": ["A", "G"], "N": ["A", "G", "C", "T"]}
+BASEMATCH_DICT = {"Y": ["C", "T"], 
+                  "R": ["A", "G"], 
+                  "N": ["A", "G", "C", "T"],
+                  "B": ["G", "C", "T"]}
 
-def is_base_match(base1: str, base2: str) -> (bool, int):    
+def is_base_match(base1: str, base2: str) -> (bool, int):
+    if base1 in BASEMATCH_DICT and base2 in BASEMATCH_DICT:
+        return False, BASEMATCH_UNCERTAIN   
+    
     if (base1 == "T" and base2 == "U") or (base2 == "T" and base1 == "U"):
-        return False, BASEMATCH_CMP_T_AND_U 
+        return False, BASEMATCH_CMP_T_AND_U   
     
     if base1 in BASEMATCH_DICT:
         if base2 in BASEMATCH_DICT[base1]:
@@ -21,8 +28,7 @@ def is_base_match(base1: str, base2: str) -> (bool, int):
 
     if base2 in BASEMATCH_DICT:
         if base1 in BASEMATCH_DICT[base2]:
-            return True, BASEMATCH_NOERROR         
-    
+            return True, BASEMATCH_NOERROR     
 
     return base1 == base2, BASEMATCH_NOERROR
 
@@ -34,10 +40,10 @@ if __name__ == "__main__":
     #print(is_base_match("G", "G"))
     #print(is_base_match("T", "T"))    
     
-    print(is_base_match("C", "Y"))  
-    print(is_base_match("C", "Y"))  
-    print(is_base_match("Y", "C"))  
-    print(is_base_match("Y", "T"))
+    print(is_base_match("B", "Y"))  
+    print(is_base_match("B", "Y"))  
+    print(is_base_match("B", "C"))  
+    print(is_base_match("B", "T"))
     
     #print(is_base_match("A", "N"), "A matches or not matches any base.")
     #print(is_base_match("X", "N"), "X matches or not matches any base.")
